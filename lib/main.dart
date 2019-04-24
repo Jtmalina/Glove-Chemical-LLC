398c6d68913983f5f2bdfcf7ca0185cc13c5d5ae
Jtmalina <jtmalina@buffalo.edu>
2019-04-23 22:10:57 -0400

    Revert "Added Textfields and Text"

--- lib/main.dart
+++ lib/main.dart
@@ -46,139 +46,6 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Glove Chemical, LLC"),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Phone Number:',
-                  style: TextStyle(
-                    fontSize: 25,
-                    //background: Paint()..color = Colors.grey
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  enableInteractiveSelection: true,
-                  autocorrect: true,
-                  autofocus: true,
-                  enabled: true,
-                  textAlign: TextAlign.center,
-                  maxLength: 13,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "(xxx)xxx-xxxx"
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'PO/DEPARTMENT:',
-                  style: TextStyle(fontSize: 25),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child:TextField(
-                  enableInteractiveSelection: true,
-                  autocorrect: true,
-                  autofocus: true,
-                  enabled: true,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "xxxx"
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child:Text(
-                  'NEW USERS:',
-                  style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Facility Name:',
-                  style: TextStyle(fontSize: 25),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  enableInteractiveSelection: true,
-                  autocorrect: true,
-                  enabled: true,
-                  autofocus: true,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "xxxx"
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Facility Address',
-                  style: TextStyle(fontSize: 25),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  enableInteractiveSelection: true,
-                  autocorrect: true,
-                  autofocus: true,
-                  enabled: true,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Address, City, State, Zip",
-                  ),
-                ),
-              ),
-
-              RaisedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => NotePage()),
-                  );
-                },
-                child: Text('Save/Send Note'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class NotePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Glove Chemical, LLC"),
-      ),
       body: Center(
         child: RaisedButton(
           onPressed: () {
@@ -190,3 +57,91 @@ class NotePage extends StatelessWidget {
     );
   }
 }
+/*class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: */ /*1*/ /* (context, i) {
+          if (i.isOdd) return Divider(); */ /*2*/ /*
+
+          final index = i ~/ 2; */ /*3*/ /*
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); */ /*4*/ /*
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(   // Add the lines from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {      // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[      // Add 3 lines from here...
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],                      // ... to here.
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(   // Add 20 lines from here...
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return Scaffold(         // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+  */ /*@override
+  Widget build(BuildContext context){
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }*/ /*
+}*/
